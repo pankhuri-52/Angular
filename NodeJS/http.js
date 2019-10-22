457169210a0f54bc3506f3f5ed0e9ee863bceffb
Pankhuri <pankhuritrikha@gmail.com>
2019-10-22 11:30:29 +0530

    Modification in file http.js

--- NodeJS/http.js
+++ NodeJS/http.js
@@ -2,6 +2,7 @@ var http=require('http');
 console.log('Server is active go to port number 9000');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/html'});
-	res.write("welcome to the environment of Node.js");
+	//res.write("welcome to the environment of Node.js");
+	res.write(req.url);
 	res.end();
 }).listen(9000);
